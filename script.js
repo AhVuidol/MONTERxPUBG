eb963eb665728e03fd4fb678384b80b419804238
AhVuidol <nhd9snvf6w@privaterelay.appleid.com>
2026-07-10 02:14:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -79,3 +79,34 @@ if (searchInput) {
     });
 
 }
+/* ===========================
+   BACK TO TOP
+=========================== */
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        backToTop.style.display = "block";
+
+    } else {
+
+        backToTop.style.display = "none";
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
