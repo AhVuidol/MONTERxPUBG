c59ff80cfdf935d4299aa890070644c7cff25a8a
AhVuidol <nhd9snvf6w@privaterelay.appleid.com>
2026-07-10 04:03:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,17 @@ backToTop.addEventListener("click", () => {
     });
 
 });
+window.addEventListener("scroll",()=>{
+
+    const winScroll=
+        document.documentElement.scrollTop;
+
+    const height=
+        document.documentElement.scrollHeight-
+        document.documentElement.clientHeight;
+
+    const scrolled=(winScroll/height)*100;
+
+    document.getElementById("scrollBar").style.width=scrolled+"%";
+
+});
