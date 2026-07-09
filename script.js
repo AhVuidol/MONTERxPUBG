5da79cf86089c11d8a2df33a6c73556acb4437e3
AhVuidol <nhd9snvf6w@privaterelay.appleid.com>
2026-07-09 14:25:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// MONTERxPUBG Script
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("closePopup");
+
+closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
+});
