1cf975207c81e74f7bf058e279f76e85e80e83be
AhVuidol <nhd9snvf6w@privaterelay.appleid.com>
2026-07-09 14:39:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 const popup = document.getElementById("popup");
-const closeBtn = document.getElementById("closePopup");
+const closePopup = document.getElementById("closePopup");
+const okPopup = document.getElementById("okPopup");
 
-closeBtn.addEventListener("click", function () {
+function closeWindow(){
     popup.style.display = "none";
+}
+
+closePopup.addEventListener("click", closeWindow);
+okPopup.addEventListener("click", closeWindow);
+
+// Ẩn popup 1 giờ
+const hideBtn = document.querySelector(".hide-btn");
+
+hideBtn.addEventListener("click", () => {
+
+    localStorage.setItem(
+        "hidePopupUntil",
+        Date.now() + 60 * 60 * 1000
+    );
+
+    closeWindow();
+
+});
+
+const hideUntil = localStorage.getItem("hidePopupUntil");
+
+if(hideUntil && Date.now() < Number(hideUntil)){
+    popup.style.display = "none";
+}
+
+// Tìm kiếm
+const search = document.getElementById("search");
+
+search.addEventListener("keyup", function(){
+
+    let value = this.value.toLowerCase();
+
+    document.querySelectorAll(".item").forEach(item=>{
+
+        if(item.innerText.toLowerCase().includes(value)){
+            item.style.display="flex";
+        }else{
+            item.style.display="none";
+        }
+
+    });
+
 });
